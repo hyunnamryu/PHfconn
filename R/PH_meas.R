d5394a99167a87cf6c738d91ed99970b142d00b9
hyunnamryu <monicaryu21@gmail.com>
2023-05-08 09:29:32 -0400

    Update PH_meas.R

--- R/PH_meas.R
+++ R/PH_meas.R
@@ -28,22 +28,22 @@ PH_meas = function(connectivity){
   L = length(W_out$WB1) # number of total cycles; N(N-1)/2-(N-1) = (N-2)(N-1)/2
 
   # mean weights of tree(MST); global weight (cost) efficiency of tree (<-> characteristic path length)
-  mB0 = mean(W_out$WB0)
+  BS = mean(W_out$WB0)
 
   # range of weights of tree(MST); global weight (cost) efficiency of tree (<-> diameter)
-  rB0 = diff(range(W_out$WB0))
+  BD = diff(range(W_out$WB0))
 
   # AUC under B1 curve until all nodes are connected
-  mB1 = mean(W_out$WB10)
+  CS = mean(W_out$WB10)
 
   # height of B1 curve until all nodes are connected - the total number of cycles when all nodes are connected
-  hB1 = length(W_out$WB10)/N
+  CF = length(W_out$WB10)/N
   #hB1 = mean(as.numeric(lapply(W_out$WB0,function(x) sum(W_out$W>=x)))/(M+L))
 
   # adjusted BD : BD*CF
   aBD = BD*CF
 
-  out = list(BS=mB0,BD=rB0,CS=mB1,CF=hB1, aBD=aBD)
+  out = list(BS=BS,BD=BD,CS=CS,CF=CF, aBD=aBD)
 
   return(out)
 }
